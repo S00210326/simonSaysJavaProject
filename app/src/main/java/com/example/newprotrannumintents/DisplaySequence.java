b11a31cd124a3c8760a5c1189b2168cb98a45556
paulmcgonigle67 <s00210326@atu.ie>
2022-12-19 20:29:16 +0000

    Updated home screen and DisplaySequence

--- app/src/main/java/com/example/newprotrannumintents/DisplaySequence.java
+++ app/src/main/java/com/example/newprotrannumintents/DisplaySequence.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +18,72 @@ import java.util.Random;
 
 public class DisplaySequence extends AppCompatActivity {
 
+    private final int BLUE = 1;
+    private final int RED = 2;
+    private final int YELLOW = 3;
+    private final int GREEN = 4;
         List<Integer> numSeq = new ArrayList<>();
         Button b1;
         Integer score;
         int arrayCheckNum;
+    int sequenceCount = 4, n = 0;
+    int[] gameSequence = new int[120];
+        Button btnUp, btnRight, btnLeft, btnDown, fb;
+
+    int arrayIndex = 0;
+    CountDownTimer ct = new CountDownTimer(6000,  1500) {
+
+        public void onTick(long millisUntilFinished) {
+            //mTextField.setText("seconds remaining: " + millisUntilFinished / 1500);
+            oneButton();
+            //here you can have your logic to set text to edittext
+        }
+
+        public void onFinish() {
+            //mTextField.setText("done!");
+            // we now have the game sequence
+
+            for (int i = 0; i< arrayIndex; i++)
+                Log.d("game sequence", String.valueOf(gameSequence[i]));
+            // start next activity
+
+            // put the sequence into the next activity
+            // stack overglow https://stackoverflow.com/questions/3848148/sending-arrays-with-intent-putextra
+            //Intent i = new Intent(A.this, B.class);
+            //i.putExtra("numbers", array);
+            //startActivity(i);
+
+            // start the next activity
+            // int[] arrayB = extras.getIntArray("numbers");
+        }
+    };
+    private void oneButton() {
+        n = getRandom(sequenceCount);
+
+        Toast.makeText(this, "Number = " + n, Toast.LENGTH_SHORT).show();
+
+        switch (n) {
+            case 1:
+                flashButton(btnUp);
+                gameSequence[arrayIndex++] = BLUE;
+                break;
+            case 2:
+                flashButton(btnRight);
+                gameSequence[arrayIndex++] = RED;
+                break;
+            case 3:
+                flashButton(btnDown);
+                gameSequence[arrayIndex++] = YELLOW;
+                break;
+            case 4:
+                flashButton(btnLeft);
+                gameSequence[arrayIndex++] = GREEN;
+                break;
+            default:
+                break;
+        }   // end switch
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +119,37 @@ public class DisplaySequence extends AppCompatActivity {
         i.putExtra("ARRAY_CHECK", arrayCheckNum);
         startActivity(i);
     }
+    private int getRandom(int maxValue) {
+        return ((int) ((Math.random() * maxValue) + 1));
+    }
     public void getAndGoHard(List<Integer> numSequence){
         Intent i = new Intent(this, AccelerometerAttempt.class);
         i.putIntegerArrayListExtra("myList",(ArrayList<Integer>) numSequence);
         i.putExtra("MY_SCORE", score);
         i.putExtra("ARRAY_CHECK", arrayCheckNum);
         startActivity(i);
+    }
+    private void flashButton(Button button) {
+        fb = button;
+        Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            public void run() {
+
+                fb.setPressed(true);
+                fb.invalidate();
+                fb.performClick();
+                Handler handler1 = new Handler();
+                Runnable r1 = new Runnable() {
+                    public void run() {
+                        fb.setPressed(false);
+                        fb.invalidate();
+                    }
+                };
+                handler1.postDelayed(r1, 600);
+
+            } // end runnable
+        };
+        handler.postDelayed(r, 600);
     }
 
 
